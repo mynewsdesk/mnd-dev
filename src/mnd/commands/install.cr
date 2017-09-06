24989a76f2ecd2170c1933265264c44e58a7720e
David Backeus <duztdruid@gmail.com>
2017-09-06 12:01:52 +0200

    Ability to install only a particular repo

--- src/mnd/commands/install.cr
+++ src/mnd/commands/install.cr
@@ -1,17 +1,22 @@
 module Mnd
   class Commands::Install < Commands::Base
     summary "Install missing repos"
-    usage "mnd install # Install and setup missing repos"
+    usage <<-EOF
+    mnd install # Install and setup missing repos
+    mnd install mynewsdesk # Only install mynewsdesk
+    EOF
+
 
     def perform
-      repos = Repo.all.reject &.exists?
+      repos = selected_repos.presence || Repo.all
+      uninstalled_repos = repos.reject &.exists?
 
-      if repos.empty?
+      if uninstalled_repos.empty?
         display.info "All repos are already installed!"
         return
       end
 
-      repos.each do |repo|
+      uninstalled_repos.each do |repo|
         display.header "Installing #{repo.name}"
 
         run "cd #{LocalConfig.instance.root_path} && git clone #{repo.git}"
