5f18cc7e611d05cbecfdaa6fc0792531bc035f1d
David Backeus <duztdruid@gmail.com>
2024-11-11 20:13:29 +0100

    Update platforms

--- src/mnd/platforms.cr
+++ src/mnd/platforms.cr
@@ -1,23 +1,12 @@
 module Mnd
   PLATFORMS = {
     "prime" => [
+      Repo.new("mnd-analyze", color: :light_red, alias: "analyze"),
       Repo.new("mnd-audience", color: :light_cyan, alias: "audience"),
-      Repo.new("mnd-events-api", color: :yellow, alias: "events-api"),
-      Repo.new("mnd-langdetect", color: :light_yellow, alias: "langdetect"),
+      Repo.new("mnd-assets", color: :light_green, alias: "assets"),
       Repo.new("mnd-publish-frontend", color: :yellow, alias: "publish-frontend"),
-      Repo.new("mnd-reader-frontend", color: :pink, alias: "reader-frontend"),
-      Repo.new("mnd-track-backend", color: :light_red, alias: "track-backend"),
       Repo.new("mynewsdesk", color: :cyan),
       Repo.new("social-media-monitor", color: :green, alias: "smm"),
     ],
-
-    "poptype" => {
-      Repo.new("mndx-analyze", color: :cyan, alias: "analyze"),
-      Repo.new("mndx-dashboard", color: :light_blue, alias: "dashboard"),
-      Repo.new("mndx-mailer", color: :green, alias: "mailer"),
-      Repo.new("mndx-web", color: :red, alias: "web"),
-      Repo.new("mndx-amqp", color: :light_red, alias: "amqp"),
-      Repo.new("mndx-mailer_client", color: :light_green, alias: "mailer_client"),
-    },
   }
 end
